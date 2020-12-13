3d1588c08d3ad3eeba3d1920fac7410bf3e75b17
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-13 08:50:07 +0200

    Learned how to implement Spring JDBC and do basic CRUD operations using Spring JDBC. Also learned how to implement cutom RowMapper

--- src/main/java/za/co/iherridge0/databasedemo/databasedemo/DatabasedemoApplication.java
+++ src/main/java/za/co/iherridge0/databasedemo/databasedemo/DatabasedemoApplication.java
@@ -1,13 +1,40 @@
 package za.co.iherridge0.databasedemo.databasedemo;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import za.co.iherridge0.databasedemo.databasedemo.entity.Person;
+import za.co.iherridge0.databasedemo.databasedemo.jdbc.PersonJdbcDao;
+
 @SpringBootApplication
-public class DatabasedemoApplication {
+public class DatabasedemoApplication implements CommandLineRunner{
+	
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	PersonJdbcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabasedemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		log.info("All users -> {}", dao.findAll());
+		log.info("User id 10001 -> {}", dao.findById(10001));
+		log.info("Deleting -> {}", dao.deleteById(10002));
+		Person person = new Person(10004, "Irwin", "SA", new Date());
+		log.info("Insert user -> {}", dao.insert(person));
+		person.setLocation("Western Cape, SA");
+		log.info("Update user -> {}", dao.update(person));
 	}
 
 }
