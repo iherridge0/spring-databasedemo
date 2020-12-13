4f084506cede00eea7d8078efba7986f2f523321
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-13 07:48:27 +0200

    commit

---
+++ src/main/java/za/co/iherridge0/databasedemo/databasedemo/DatabasedemoApplication.java
@@ -0,0 +1,13 @@
+package za.co.iherridge0.databasedemo.databasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/iherridge0/databasedemo/databasedemo/DatabasedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.iherridge0.databasedemo.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
